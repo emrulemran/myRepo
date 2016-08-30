2ba7bc74753bef9b7d9f17b06a4509e97ee76e18
RUMA <ruma@RUMAs-MacBook-Pro.local>
2016-08-30 19:03:28 -0400

    another line added

--- New.java
+++ New.java
@@ -1,7 +1,9 @@
 public class New{
 	public static void main (String[]args){
 	int i = 100;
-	System.out.println(i);
-	
+	int j = 200;
+	int sum = i+j;
+	System.out.println(sum);
+	System.out.println("This is a simple addition");
 	}
 }
