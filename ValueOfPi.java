f28f437a7796e08ed2234cc84365e65a778e18eb
EMRUL HASAN <emrulemran@gmail.com>
2016-10-17 17:35:34 -0400

    ValueOfPi.java file added

---
+++ ValueOfPi.java
@@ -0,0 +1,20 @@
+package maths.practice;
+
+public class ValueOfPi {
+	public static void main(String[] args) {
+
+		double valueOfPi = Math.E;
+		double valueOfe = Math.PI;
+		
+		
+		double aNo = 2.8234;
+		aNo = Math.floor(aNo);
+		aNo = Math.ceil(aNo);		
+
+		System.out.println("The value of Pi is: "+valueOfPi);
+		System.out.println("The value of e is: "+valueOfe);
+		System.out.println(aNo);
+	
+		
+	}
+}
