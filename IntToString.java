1e244139baf6cef64ee1d1f6251d5dbf3acda631
EMRUL HASAN <emrulemran@gmail.com>
2016-10-28 11:33:29 -0400

    new files added

---
+++ IntToString.java
@@ -0,0 +1,12 @@
+package basic.practice;
+
+public class IntToString {
+	public static void main(String[] args) {
+		int sampleNumber = 123;
+
+		// Convert integer to string
+		String stringForNumber = Integer.toString(sampleNumber);
+
+		System.out.println(stringForNumber);
+	}
+}
